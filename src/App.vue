2ef0a32cfff8fa648000e14828a91caf9d1140ff
Noldor5150 <ruikis.paulius@gmail.com>
2019-09-12 20:31:13 +0300

    ascending descending functions

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
   <div class="app">
     <div class="surface">
       <h1 class="headline">
-        <i class="material-icons">
-          assignment_turned_in
-        </i>
+        <i class="material-icons">assignment_turned_in</i>
         Todo List
-        <BaseButton>All</BaseButton>
-        <BaseButton>Completed</BaseButton>
+        <BaseButton
+          v-for="filter in filters"
+          :key="filter"
+          :oulined="filter !==activeFilter"
+          class="surface__filter"
+          @click="addFilter(filter)"
+        >{{filter}}</BaseButton>
+        <BaseButton
+          v-for="type in sortedTypes"
+          :key="type"
+          :oulined="type !==sortedBy"
+          class="surface__filter"
+          @click="setSortedBy(type)"
+        >{{type}}</BaseButton>
       </h1>
-      <h4 class="headline headline--4"> {{ today }} </h4>
+      <h4 class="headline headline--4">{{ today }}</h4>
 
       <div class="surface__new">
         <BaseTextField
@@ -18,21 +28,15 @@
           class="surface__input"
           @keydown.enter="handleClick"
         />
-        <BaseButton
-          class="surface__button"
-          @click="handleClick"
-        >
-            <i class="material-icons"> playlist_add </i>
+        <BaseButton class="surface__button" @click="handleClick">
+          <i class="material-icons">playlist_add</i>
         </BaseButton>
       </div>
 
-      <div
-        v-if="todos.length > 0"
-        class="surface__todos"
-      >
+      <div v-if="todos.length > 0" class="surface__todos">
         <TodoList>
           <TodoListItem
-            v-for="todo in todos"
+            v-for="todo in filteredTodos"
             :key="todo.id"
             :todo="todo"
             @change="handleChange(todo.id, $event)"
@@ -41,120 +45,151 @@
         </TodoList>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import uniqid from 'uniqid'
-import BaseButton from '@/components/BaseButton'
-import BaseTextField from '@/components/BaseTextField'
-import TodoList from '@/components/TodoList'
-import TodoListItem from '@/components/TodoListItem'
+import uniqid from "uniqid";
+import BaseButton from "@/components/BaseButton";
+import BaseTextField from "@/components/BaseTextField";
+import TodoList from "@/components/TodoList";
+import TodoListItem from "@/components/TodoListItem";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     BaseButton,
     BaseTextField,
     TodoList,
     TodoListItem
   },
-  data () {
+  data() {
     return {
+      activeFilter: "all",
+      sortedBy: "asc",
+      sortedTypes: ["asc", "desc"],
+      filters: ["all", "completed", "inprogress"],
       newTodoText: null,
       todos: []
-    }
+    };
   },
   computed: {
-    totalTodos () {
-      return this.todos.length
+    filteredTodos() {
+      const filteredTodos = this.todos.filter(todo => {
+        switch (this.activeFilter) {
+          case "all":
+            return true;
+          case "completed":
+            return todo.done;
+          case "inprogress":
+            return !todo.done;
+          default:
+            return true;
+        }
+      });
+      if (this.sortedBy === "asc") {
+        return filteredTodos.sort((a, b) => a.time - b.time);
+      }
+      if (this.sortedBy === "desc") {
+        return filteredTodos.sort((a, b) => b.time - a.time);
+      }
+      return filteredTodos;
     },
-    today () {
-      return new Date().toLocaleString('en-GB', { weekday: 'long', month: 'long', day: '2-digit' })
+
+    today() {
+      return new Date().toLocaleString("en-GB", {
+        weekday: "long",
+        month: "long",
+        day: "2-digit"
+      });
     }
   },
 
   methods: {
-    handleClick (event) {
-      if (!this.newTodoText) return
+    addFilter(value) {
+      this.activeFilter = value;
+    },
+    setSortedBy(type) {
+      this.sortedBy = type;
+    },
+    handleClick(event) {
+      if (!this.newTodoText) return;
       const todo = {
         id: uniqid(),
         time: Date.now(),
         text: this.newTodoText,
         done: false
-      }
-      this.todos = [...this.todos, todo]
-      this.newTodoText = null
+      };
+      this.todos = [...this.todos, todo];
+      this.newTodoText = null;
     },
 
-    handleChange (id, event) {
+    handleChange(id, event) {
       this.todos = this.todos.map(todo => {
         if (todo.id === id) {
-          todo.done = event.target.checked
+          todo.done = event.target.checked;
         }
-        return todo
-      })
+        return todo;
+      });
     },
 
-    handleDelete (id) {
-      this.todos = this.todos
-        .filter(todo => todo.id !== id)
+    handleDelete(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
-  .app {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100%;
-    padding: 1.5rem;
+.app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100%;
+  padding: 1.5rem;
+}
+
+.headline {
+  display: flex;
+  align-items: center;
+  font-size: 1.75rem;
+  margin: 0;
+  margin-bottom: 1.5rem;
+
+  &--4 {
+    font-size: 1.15rem;
+    font-weight: normal;
+    color: rgba(black, 0.51);
   }
 
-  .headline {
+  .material-icons {
+    font-size: 2.5rem;
+    margin-right: 0.5rem;
+  }
+}
+
+.surface {
+  background-color: white;
+  padding: 2rem;
+  border-radius: 4px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+  max-width: 600px;
+  width: 100%;
+
+  &__new {
     display: flex;
-    align-items: center;
-    font-size: 1.75rem;
-    margin: 0;
     margin-bottom: 1.5rem;
-
-    &--4 {
-      font-size: 1.15rem;
-      font-weight: normal;
-      color: rgba(black, 0.51);
-    }
-
-    .material-icons {
-      font-size: 2.5rem;
-      margin-right: 0.5rem;
-    }
   }
 
-  .surface {
-    background-color: white;
-    padding: 2rem;
-    border-radius: 4px;
-    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
-    max-width: 600px;
-    width: 100%;
-
-    &__new {
-      display: flex;
-      margin-bottom: 1.5rem;
-    }
-
-    &__input {
-      border-top-right-radius: 0;
-      border-bottom-right-radius: 0;
-      border-right: 0;
-    }
-
-    &__button {
-      border-top-left-radius: 0;
-      border-bottom-left-radius: 0;
-    }
+  &__input {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    border-right: 0;
   }
+
+  &__button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+}
 </style>
